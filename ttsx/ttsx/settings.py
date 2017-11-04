1df9ad557b4b1f29b8533bfabedf6c2e5f86df4e
xiaoye <jkl00442@126.com>
2017-11-04 14:55:15 +0800

    DEBUG=False

--- ttsx/ttsx/settings.py
+++ ttsx/ttsx/settings.py
@@ -77,7 +77,6 @@ WSGI_APPLICATION = 'ttsx.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': '',
         'USER': 'root',
         'PASSWORD': '987844174',
